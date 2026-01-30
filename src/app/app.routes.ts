e74510c477b9caefcbf88601c9e1fa502f0852fe
Agustin Farisano <afarisano@bitbucket.org>
2026-01-30 11:58:04 -0300

    quito error en merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { HomePage } from './shared/pages/home-page/home-page';
-=======
 import { HomePage } from './country/shared/pages/home-page/home-page';
->>>>>>> 31a2ae41d6cfbb445de99d5245a6828761befe1c
 
 export const routes: Routes = [
   {
--- src/app/country/pages/by-country-page/country-page.ts
+++ src/app/country/pages/by-country-page/country-page.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { SearchInputComponent } from '../../components/country-search-input/country-search-input';
-import { CountryList } from '../../components/country-list/country-list';
 import { CountryService } from '../../services/country';
 
 @Component({
   selector: 'app-country-page',
-  imports: [SearchInputComponent, CountryList],
+  imports: [],
   templateUrl: './country-page.html',
   styleUrl: './country-page.css',
 })
