8ee9706d2f30235e93a1219101a309291134687b
Agustin Farisano <afarisano@bitbucket.org>
2026-01-30 14:50:43 -0300

    agrego rutas nuevas

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
 import { CountryPage } from './pages/by-country-page/country-page';
 import { CountryLayout } from './layouts/country-layout/country-layout';
+import { ByRegionPage } from './pages/by-region-page/by-region-page';
 
 export const countryRoutes: Routes = [
   {
@@ -12,6 +13,15 @@ export const countryRoutes: Routes = [
         path: 'by-capital',
         component: ByCapitalPage,
       },
+      {
+        path: 'by-country',
+        component: CountryPage,
+      },
+      {
+        path: 'by-region',
+        component: ByRegionPage,
+      },
+
       {
         path: 'by/:code',
         component: CountryPage,
