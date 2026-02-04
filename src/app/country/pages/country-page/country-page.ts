28a18de6b3341c954eb92f194b22272c5bb12a62
Agustin Farisano <afarisano@bitbucket.org>
2026-02-04 09:05:46 -0300

    agrego informacion del pais

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -3,11 +3,14 @@ import { firstValueFrom } from 'rxjs'; // Para convertir el observable
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { NotFound } from '../../shared/components/not-found/not-found';
+import { CountryInformation } from './country-information/country-information';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.html',
   styleUrl: './country-page.scss',
+  imports: [NotFound, CountryInformation],
 })
 export class CountryPage {
   countryCode = inject(ActivatedRoute).snapshot.params['code'];
